efe245128d7d8ab0790bb097e23aa91a7c921f76
camilaonosky <camilaonosky@gmail.com>
2025-09-02 14:15:44 -0300

    Agrego mensaje contacto al admin

--- bloginfo2025/apps/blog/admin.py
+++ bloginfo2025/apps/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Articulo, Comentario, Categoria
+from .models import Articulo, Comentario, Categoria, Mensaje_Contacto
 
 # Register your models here.
 
 admin.site.register(Articulo)
 admin.site.register(Comentario)
 admin.site.register(Categoria)
+admin.site.register(Mensaje_Contacto)
